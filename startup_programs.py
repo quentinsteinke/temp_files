5460893f95688b55cb3d2619b51a03f36b5af5c5
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-22 16:52:14 -0400

    trimming files

--- startup_programs.py
+++ startup_programs.py
@@ -1,9 +1,9 @@
 import os
 
-discord = R"'C:\Users\Quentin Steinke\AppData\Local\Discord\app-1.0.9006\Discord.exe'"
-outlook = R"'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE'"
-teams = R"'C:\Users\Quentin Steinke\AppData\Local\Microsoft\Teams\current\Teams.exe'"
-chrome = R"'C:\Program Files\Google\Chrome\Application\chrome.exe'"
+discord = "C:\\Users\\Quentin Steinke\\AppData\\Local\\Discord\\app-1.0.9006\\Discord.exe"
+outlook = "C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE"
+teams = "C:\\Users\\Quentin Steinke\\AppData\\Local\\Microsoft\\Teams\\current\\Teams.exe"
+chrome = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 code = "code"
 blender = "blender"
 
@@ -19,5 +19,5 @@ programs = [
 
 # Launching programs one by one
 for pro in programs:
-    os.system("start " + pro)
+    os.system('"' + pro + '"')
 #    print("start " + pro)
