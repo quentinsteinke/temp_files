dfb9152a122a5f1b94d5acc9ba64f4367ca94fb9
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-25 14:19:35 -0400

    Updating image converter

---
+++ image_converter.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import os
+
+image_path = ".\\convert\\"
+image_path_1 = "\\convert\\"
+print(image_path, image_path_1)
+
+# print(os.listdir(image_path))
+images = os.listdir(image_path)
+new_name = "Converted"
+
+print("Change name? y/n: ")
+convert_name = input()
+
+if convert_name == "y":
+    print("New file name: ")
+    new_name = input()
+    num = 1
+
+    for i in images:
+        i = image_path_1 + i
+        im = Image.open(i)
+        con_im = im.convert("RGB")
+        con_im.save(new_name + "_" + str(num) + ".jpg")
+        num += 1
+
+if convert_name == "n":
+    pass
+
+for i in images:
+    i = image_path_1 + i
+    im = Image.open(i)
+    con_im = im.convert("rgb")
+    con_im.save(im.info("filename") + ".jpg")
