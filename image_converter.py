f66f1773d1e17e6871e126be439cfef7f4230251
quentinsteinke <quentinsteinke@gmail.com>
2022-08-25 21:25:23 -0400

    bug fixes

--- image_converter.py
+++ image_converter.py
@@ -1,28 +1,31 @@
 from PIL import Image
 import os
-"""
-This script converts .png image files into .jpg files.
-It needs a subfolder named "convert" with the images you want to convert in it.
-"""
+
 image_path = ".\\convert\\"
-make_converted_dir = os.makedirs("converted")
-try:
-    make_converted_dir
-except FileExistsError:
-    pass
-new_name = "Converted"
+new_name = "Converted_Image"
+make_converted_dir = "no value set"
+conversion_type = "RGB"
 
 # See if you need forward-slash or back-slash for you os
 try:
     images = os.listdir(image_path)
+    make_converted_dir = ".\\converted\\"
 except FileNotFoundError:
     image_path = "./convert/"
     images = os.listdir(image_path)
+    make_converted_dir = "./converted/"
+    print("Converting " + str(len(images)) + " images", images)
 else:
     print("No 'convert' folder found")
 
 print("Change name? y/n: ")
 convert_name = input()
+
+try:
+    os.makedirs("converted")
+except FileExistsError:
+    pass
+
 
 if convert_name == "y":
     print("New file name: ")
@@ -32,8 +35,8 @@ if convert_name == "y":
     for i in images:
         i = image_path + i
         im = Image.open(i)
-        con_im = im.convert("RGB")
-        con_im.save(new_name + "_" + str(num) + ".jpg")
+        con_im = im.convert(conversion_type)
+        con_im.save(make_converted_dir + new_name + "_" + str(num) + ".jpg")
         num += 1
 
 if convert_name == "n":
@@ -43,7 +46,8 @@ if convert_name == "n":
         original_name = im.filename
         original_name = original_name.replace(image_path, "")
         original_name = original_name.replace(".png", "")
-        con_im = im.convert("RGB")
-        con_im.save(original_name + ".jpg")
+        con_im = im.convert(conversion_type)
+        print(make_converted_dir)
+        con_im.save(make_converted_dir + original_name + ".jpg")
 
 print("All done!")
